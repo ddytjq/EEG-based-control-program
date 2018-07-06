1e98f86b300ec46a008e7bb523f4b8b09e5358bb
ddytjq <dytjqg@gmail.com>
2018-07-06 17:15:08 +0900

    normolization test

--- Classfication/src/Main.java
+++ Classfication/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Random;
 
 public class Main {
+	
+	private static double[][] dataset = new double[5][5];
+	private static Random random = new Random();
+
 	public static void main(String[] args) {
-		Normalization nor = new Normalization(null, 0, 0);
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				dataset[i][j] = random.nextInt(100);
+			}
+		}
+		
+		Normalization no = new Normalization(dataset, 0, 1);
 		
 	}
 
