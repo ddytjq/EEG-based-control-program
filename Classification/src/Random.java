791acbc8cc3e7e32d182a90eb21077ab9ed290cb
ddytjq <dytjqg@gmail.com>
2018-07-10 21:59:07 +0900

    데이터셋 그래프화 진행중

--- Classification/src/Random.java
+++ Classification/src/Random.java
@@ -6,16 +6,34 @@ public class Random {
 	public static void main(String[] args) throws IOException {
 
 		FileOutputStream data = new FileOutputStream(
-				"/Users/kimyosub/eclipse-workspace/EEG-based-control-program/Classification/rand/rand.txt",false);
+				"/Users/kimyosub/eclipse-workspace/EEG-based-control-program/Classification/rand/result.txt", false);
 		DataOutputStream rand = new DataOutputStream(data);
-		float result;
-		
+		String result;
+
 		for (int i = 0; i < 15000; i++) {
-			result = (float) (Math.random()*(60-45+1)+45);
-			rand.writeFloat(result);
+			result = ((String.valueOf((float) (Math.random() * (60 - 45 + 1) + 45))));
+			rand.writeBytes(result + "\n");
 			System.out.println(result);
-			result=0;	
+			result = null;
 		}
 		data.close();
+
+
+		String[] name = { "1", "2", "3", "4", "5", "6" };
+		for (int j = 0; j < name.length; j++) {
+			FileOutputStream data2 = new FileOutputStream(
+					"/Users/kimyosub/eclipse-workspace/EEG-based-control-program/Classification/rand/"+name[j]+".txt", false);
+			DataOutputStream rand2 = new DataOutputStream(data2);
+			String result2;
+			for (int i = 0; i < 15000; i++) {
+				result2 = ((String.valueOf((float) (Math.random() * 44.99999 - 15 + 1) + 15)));
+				rand2.writeBytes(result2 + "\n");
+				System.out.println(result2);
+				result2 = null;
+			}
+			data2=null;
+			rand2=null;
+			data.close();
+		}
 	}
 }
