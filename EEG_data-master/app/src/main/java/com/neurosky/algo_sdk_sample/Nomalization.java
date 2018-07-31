ff420919354e352bccb13cf922482f74c7cd0f24
ddytjq <dytjqg@gmail.com>
2018-07-31 23:50:50 +0900

    안드수정

--- EEG_data-master/app/src/main/java/com/neurosky/algo_sdk_sample/Nomalization.java
+++ EEG_data-master/app/src/main/java/com/neurosky/algo_sdk_sample/Nomalization.java
@@ -1,8 +1,17 @@
 package com.neurosky.algo_sdk_sample;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class Nomalization {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
     // 최소값 출력
     public static Double calc_min(Double age[]) {
         int i, j;
@@ -43,6 +52,34 @@ public class Nomalization {
 
     // 디비 연동 후 데이터 값 넣으면 됨
     public void main(String[] args) {
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener(){
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (int s = 18; s < 30; s++) {
+                    for (DataSnapshot snapshot : dataSnapshot.child("2018년").child("05월").child("13일").child("19시").child("58분").child(s + "초").child("Alpha").getChildren()) {
+                        result_alpha.add(snapshot.getValue().toString());
+                    }
+                    for (DataSnapshot snapshot : dataSnapshot.child("2018년").child("05월").child("13일").child("19시").child("58분").child(s + "초").child("Beta").getChildren()) {
+                        result_low_beta.add(snapshot.getValue().toString());
+                    }
+                    for (DataSnapshot snapshot : dataSnapshot.child("2018년").child("05월").child("13일").child("19시").child("58분").child(s + "초").child("Delta").getChildren()) {
+                        result_delta.add(snapshot.getValue().toString());
+                    }
+                    for (DataSnapshot snapshot : dataSnapshot.child("2018년").child("05월").child("13일").child("19시").child("58분").child(s + "초").child("Gamma").getChildren()) {
+                        result_gamma.add(snapshot.getValue().toString());
+                    }
+                    for (DataSnapshot snapshot : dataSnapshot.child("2018년").child("05월").child("13일").child("19시").child("58분").child(s + "초").child("Theta").getChildren()) {
+                        result_theta.add(snapshot.getValue().toString());
+                    }
+                    double hb1 = rand.nextInt(40 - 25 + 1) + 25;
+                    double smr1 = rand.nextInt(40 - 25 + 1) + 25;
+                    result_high_beta.add(String.valueOf(hb1));
+                    result_smr.add(String.valueOf(smr1));
+                    hb1 = 0;
+                    smr1 = 0;
+                }
+            }})}
+
 
         ArrayList<String> result = new ArrayList<>();
         Double[] age = new Double[result.size()];
